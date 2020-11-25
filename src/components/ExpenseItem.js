ce7a2f14e922de88f9c4fcd69a0f992238a76f5e
gokhandogan7 <gdogan3746@gmail.com>
2020-11-25 21:22:36 +0100

    expense items created with staylings

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from "react";
+import { MdDelete, MdEdit } from "react-icons/md";
 
-export const ExpenseItem = ({item}) => {
-    
-    return (
-        <div>
-        the amount of {item.charge} {item.amount}
-        </div>
-    )
-}
+export const ExpenseItem = ({ item }) => {
+  return (
+    <li className="item">
+      <div className="info">
+        <span className="expense">{item.charge}</span>
+        <span className="amount">${item.amount}</span>
+      </div>
+      <div>
+        <button className="clear-btn">
+          {" "}
+          <MdDelete />{" "}
+        </button>
+        <button className="edit-btn">
+          {" "}
+          <MdEdit />{" "}
+        </button>
+      </div>
+    </li>
+  );
+};
